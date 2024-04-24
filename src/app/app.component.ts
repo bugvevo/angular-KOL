759c0b4f08db967912545e0ee4946f6d0e49f3ab
Isabel Skidmore <isabelskidmore@gmail.com>
2024-04-23 20:33:49 -0500

    Add global styles and colors. Switch generic components to use Sass. Create home component scaffolding.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-test';
+	title = 'k-o-l';
+	browserOpen = false;
+
+	desktopIcons = [
+		{
+			name: 'one',
+			imageSrc: './assets/images/BrowserIcon.png',
+		},
+		{
+			name: 'two',
+			imageSrc: './assets/images/BrowserIcon.png',
+		},
+		{
+			name: 'Browser',
+			imageSrc: './assets/images/BrowserIcon.png',
+		},
+	];
+
+	clickShortcut(shortcutName: string) {
+		switch (shortcutName) {
+			case 'one': {
+				console.log('you clicked one!');
+				break;
+			}
+			case 'two': {
+				console.log('you clicked two!');
+				break;
+			}
+			case 'Browser': {
+				this.clickBrowserIcon();
+				break;
+			}
+		}
+	}
+
+	clickBrowserIcon() {
+		if (this.browserOpen) {
+			return;
+		} else {
+			this.browserOpen = true;
+		}
+	}
+
+	onStartClick() {
+		alert('hello');
+	}
 }
--- src/app/inner-browser/inner-browser.component.ts
+++ src/app/inner-browser/inner-browser.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-inner-browser',
-  templateUrl: './inner-browser.component.html',
-  styleUrls: ['./inner-browser.component.css']
+	selector: 'app-inner-browser',
+	templateUrl: './inner-browser.component.html',
+	styleUrls: ['./inner-browser.component.scss'],
 })
-export class InnerBrowserComponent {
-
-}
+export class InnerBrowserComponent {}
